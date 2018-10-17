d7db1e789bd31847693d06781f96c327d0df585b
ATILLA ARSLAN <atillaa@rocketmail.com>
2018-10-17 22:40:29 +0000

    automatically backed up by learn

--- test/index-test.js
+++ test/index-test.js
@@ -25,6 +25,11 @@ describe('logShout(string)', function() {
   })
 })
 
+function sayHiToGrandma(string) {
+  if (string.toLowerCase() === string) {
+    return "I can\'t hear you!";
+  } 
+
 describe('logWhisper(string)', function() {
   it('calls console.log() its one argument in all lowercase', function() {
     const spy = expect.spyOn(console, 'log').andCallThrough()
