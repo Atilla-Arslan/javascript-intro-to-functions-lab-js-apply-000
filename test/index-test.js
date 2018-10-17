853478879c006904e7b7f5c5eaa775df893314fb
ATILLA ARSLAN <atillaa@rocketmail.com>
2018-10-17 22:41:50 +0000

    automatically backed up by learn

--- test/index-test.js
+++ test/index-test.js
@@ -17,29 +17,10 @@ function sayHiToGrandma(string) {
   if (string.toLowerCase() === string) {
     return "I can\'t hear you!";
   } 
-
-describe('logWhisper(string)', function() {
-  it('calls console.log() its one argument in all lowercase', function() {
-    const spy = expect.spyOn(console, 'log').andCallThrough()
-
-    logWhisper('HELLO')
-
-    expect(spy).toHaveBeenCalledWith('hello')
-
-    console.log.restore()
-  })
-})
-
-describe('sayHiToGrandma(string)', function() {
-  it('returns "I can\'t hear you!" if `string` is lowercase', function() {
-    expect(sayHiToGrandma('hello')).toEqual("I can't hear you!")
-  })
-
-  it('returns "YES INDEED!" if `string` is uppercase', function() {
-    expect(sayHiToGrandma('HELLO')).toEqual("YES INDEED!")
-  })
-
-  it('returns "I love you, too." if `string` is "I love you, Grandma."`', function() {
-    expect(sayHiToGrandma("I love you, Grandma.")).toEqual("I love you, too.")
-  })
-})
+  if (string.toUpperCase() === string) {
+    return "YES INDEED!";
+  }
+  if (string ==="I love you, Grandma."){
+    return "I love you, too.";
+  }
+}
