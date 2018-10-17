1904dc7db1337d9209b7bea8e6050d5438b92ded
ATILLA ARSLAN <atillaa@rocketmail.com>
2018-10-17 22:29:04 +0000

    automatically backed up by learn

--- test/index-test.js
+++ test/index-test.js
@@ -5,8 +5,8 @@ function shout(string) {
   return string.toLowerCase()
 }
 
-function shout(string) {
-  return string.toUpperCase()
+function logShout(string) {
+  console.log (string.toUpperCase())
 }
 
 describe('logShout(string)', function() {
