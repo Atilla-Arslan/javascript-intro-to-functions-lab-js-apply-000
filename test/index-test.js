595a0969767d6721ed968b93b845565962e506f3
ATILLA ARSLAN <atillaa@rocketmail.com>
2018-10-17 22:05:27 +0000

    automatically backed up by learn

--- test/index-test.js
+++ test/index-test.js
@@ -1,6 +1,6 @@
 
 describe('shout(string)', function() {
-  it('receives one argument and returns it in all caps', function() {
+   return string.toUpperCase()}, function() {
     expect(shout('hello')).toEqual('HELLO')
   })
 })
